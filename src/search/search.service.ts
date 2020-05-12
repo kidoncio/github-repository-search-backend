7c88565243274a19326fc777c4a8b07f4571aa64
Kidoncio <lucas.kidoncio@gmail.com>
2020-05-12 11:20:56 -0300

    Fix query

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -5,17 +5,35 @@ import { SearchResponse } from './interfaces/search-response.interface';
 
 @Injectable()
 export class SearchService {
+  private readonly url: string = 'https://api.github.com/search/repositories';
+
   constructor(private httpService: HttpService) {}
 
   async get(searchDto: SearchDto): Promise<SearchResponse> {
     let query = '';
 
-    query += `${searchDto.name}`;
-    query += `+language=${searchDto.language}`;
-    query += `&order=${searchDto.order}`;
+    if (searchDto.name) query += `${searchDto.name}`;
+
+    if (searchDto.language) query += `+language=${searchDto.language}`;
+
+    if (searchDto.order) {
+      if (query) {
+        query += '&';
+      }
+
+      query += `order=${searchDto.order}`;
+
+      if (searchDto.sort) {
+        query += `&sort=${searchDto.sort}`;
+      }
+    }
+
+    let apiUrl: string = this.url;
+
+    if (query) apiUrl += `?q=${query}`;
 
     const response: SearchResponse = await this.httpService
-      .get(`https://api.github.com/search/repositories?q=${query}`)
+      .get(apiUrl)
       .pipe(map(resp => resp.data))
       .toPromise();
 
