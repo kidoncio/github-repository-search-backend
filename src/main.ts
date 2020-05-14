3ac84a7d1dc402429d4fc5b7e3ae31afd1c36264
Kidoncio <lucas.kidoncio@gmail.com>
2020-05-14 11:22:34 -0300

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -23,18 +23,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/', app, document);
 
-  const whitelist: string[] = [
-    'http://localhost:3000',
-    'http://localhost:8080',
-    'https://github-repository-search-backend.now.sh/',
-  ];
   const corsOptions: CorsOptions = {
     origin: (origin: any, callback: any) => {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
+      callback(null, true);
     },
     credentials: true,
   };
